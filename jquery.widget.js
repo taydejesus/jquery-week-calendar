090d919b4742eb978452a63fb83bb4d79eaf995b
Uppala <tuj703@dca904764ada.cof.ds.capitalone.com>
2017-08-10 15:14:52 -0400

    fixed calendar population

--- jquery.widget.js
+++ jquery.widget.js
@@ -1,24 +1,10 @@
-
-  var year = new Date().getFullYear();
-  var month = new Date().getMonth();
-  var day = new Date().getDate();
-  var i = 0;
-
-
-  var eventData = {
-    events : [
-      {'id':1, 'start': new Date(year, month, day, 12), 'end': new Date(year, month, day, 13, 35),'title':'Lunch with Mike'},
-      {'id':2, 'start': new Date(year, month, day, 14), 'end': new Date(year, month, day, 14, 45),'title':'Dev Meeting'},
-      {'id':3, 'start': new Date(year, month, day + 1, 18), 'end': new Date(year, month, day + 1, 18, 45),'title':'Hair cut'},
-      {'id':4, 'start': new Date(year, month, day - 1, 8), 'end': new Date(year, month, day - 1, 9, 30),'title':'Team breakfast'},
-      {'id':5, 'start': new Date(year, month, day + 1, 14), 'end': new Date(year, month, day + 1, 15),'title':'Product showcase'}
-    ]
-  };
-
   var CLIENT_ID = '1085272513665-952icm7bd2pfq8frcb0pgav6rr41u3vj.apps.googleusercontent.com';
-
       // Array of API discovery doc URLs for APIs used by the quickstart
-    var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+  var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+var calFlag = false;
+var calendarEvents = {
+  events: []
+};
 
       // Authorization scopes required by the API; multiple scopes can be
       // included, separated by spaces.
@@ -110,66 +96,97 @@
           'orderBy': 'startTime'
         }).then(function(response) {
           var events = response.result.items;
+          console.log("EVENTS");
+          console.log(events);
           appendPre('Upcoming events:');
+
+         var year = new Date().getFullYear();
+          var month = new Date().getMonth();
+          var day = new Date().getDate();
 
           if (events.length > 0) {
             for (i = 0; i < events.length; i++) {
+              var tempObject = {};
               var event = events[i];
-              var when = event.start.dateTime;
-              if (!when) {
-                when = event.start.date;
+              var summary = event.summary;
+              var start = new Date(event.start.dateTime); 
+              var end = new Date(event.end.dateTime); 
+              var startTime = undefined;
+              var endTime = undefined;
+              if(!start) {
+                start = event.start.date;
+              } else {
+               startTime = new Date(start.getFullYear(), start.getMonth(), start.getDate(), start.getHours(), start.getMinutes())
+               endTime = new Date(end.getFullYear(), end.getMonth(), end.getDate(), end.getHours(), end.getMinutes())
               }
-              appendPre(event.summary + ' (' + when + ')')
+
+              tempObject.id = i+1;
+              tempObject.start = startTime;
+              tempObject.end = endTime;
+              tempObject.title = summary;
+              calendarEvents.events.push(tempObject);
+              
+              console.log(summary);
+              console.log(startTime);
+              console.log(endTime);
+              appendPre(event.summary + ' (' + start + ')')
             }
           } else {
             appendPre('No upcoming events found.');
           }
+          console.log(typeof(calendarEvents));
+          console.log(calendarEvents);
+          makeCal();
         });
       }
-
-  $(document).ready(function() {
-    handleClientLoad();
-    $('#calendar').weekCalendar({
-      timeslotsPerHour: 6,
-      timeslotHeigh: 30,
-      hourLine: true,
-      data: eventData,
-      height: function($calendar) {
-        return $(window).height() - $('h1').outerHeight(true);
-      },
-      eventRender : function(calEvent, $event) {
-        if (calEvent.end.getTime() < new Date().getTime()) {
-          $event.css('backgroundColor', '#aaa');
-          $event.find('.time').css({'backgroundColor': '#999', 'border':'1px solid #888'});
+ function makeCal() {
+      $('#calendar').weekCalendar({
+        timeslotsPerHour: 6,
+        timeslotHeigh: 30,
+        hourLine: true,
+        data: calendarEvents,
+        height: function($calendar) {
+          return $(window).height() - $('h1').outerHeight(true);
+        },
+        eventRender : function(calEvent, $event) {
+          if (calEvent.end.getTime() < new Date().getTime()) {
+            $event.css('backgroundColor', '#aaa');
+            $event.find('.time').css({'backgroundColor': '#999', 'border':'1px solid #888'});
+          }
+        },
+        eventNew: function(calEvent, $event) {
+          displayMessage('<strong>Added event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
+          alert('You\'ve added a new event. You would capture this event, add the logic for creating a new event with your own fields, data and whatever backend persistence you require.');
+        },
+        eventDrop: function(calEvent, $event) {
+          displayMessage('<strong>Moved Event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
+        },
+        eventResize: function(calEvent, $event) {
+          displayMessage('<strong>Resized Event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
+        },
+        eventClick: function(calEvent, $event) {
+          displayMessage('<strong>Clicked Event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
+        },
+        eventMouseover: function(calEvent, $event) {
+          displayMessage('<strong>Mouseover Event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
+        },
+        eventMouseout: function(calEvent, $event) {
+          displayMessage('<strong>Mouseout Event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
+        },
+        noEvents: function() {
+          displayMessage('There are no events for this week');
         }
-      },
-      eventNew: function(calEvent, $event) {
-        displayMessage('<strong>Added event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
-        alert('You\'ve added a new event. You would capture this event, add the logic for creating a new event with your own fields, data and whatever backend persistence you require.');
-      },
-      eventDrop: function(calEvent, $event) {
-        displayMessage('<strong>Moved Event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
-      },
-      eventResize: function(calEvent, $event) {
-        displayMessage('<strong>Resized Event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
-      },
-      eventClick: function(calEvent, $event) {
-        displayMessage('<strong>Clicked Event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
-      },
-      eventMouseover: function(calEvent, $event) {
-        displayMessage('<strong>Mouseover Event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
-      },
-      eventMouseout: function(calEvent, $event) {
-        displayMessage('<strong>Mouseout Event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
-      },
-      noEvents: function() {
-        displayMessage('There are no events for this week');
-      }
-    });
-
-    function displayMessage(message) {
+      });
+    }
+      function displayMessage(message) {
       $('#message').html(message).fadeIn();
     }
+  $(document).ready(function() {
+
+    handleClientLoad();
+
+
+   
 
     $('<div id="message" class="ui-corner-all"></div>').prependTo($('body'));
   });
